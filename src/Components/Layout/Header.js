739bb9c86e09b3724671e2a311ce6e754b3339a0
vilas Gannaram <vilasgannaram@gmail.com>
2022-07-08 13:08:49 +0530

    added and modified cart modal state

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -5,12 +5,12 @@ import HeaderCartButton from "./HeaderCartButton"
 import mealsImage from "../../assets/meals.jpg"
 import classes from "./Header.module.css"
 
-const Header = () => {
+const Header = props => {
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>React Meals</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div>
                 <img
